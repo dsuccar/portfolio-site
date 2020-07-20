a9b487956606f56c67318f2528cf286510ad644a
dsuccar <danielsuccar@icloud.com>
2020-07-20 14:00:41 -0400

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+class NavBar extends Component {
+
+
+  render(){
+    // console.log(this.props.user)
+    return(
+      <div>
+
+        "Whats New Boogaloo?"
+          {/* <Header style={this.title} onClick={this.headerClick}>
+            
+          <h1>Alphabet Aerobics</h1>
+          <img src={'/images/Logo.png'} alt="AA Logo" width="500" height="600"></img>
+     </Header>
+        <Menu>
+            <Menu.Item header>
+            
+            </Menu.Item>
+            
+            <Menu.Menu position="left">
+              <Menu.Item as={Link} to="/select_rapper">
+                  See Rappers
+              </Menu.Item>
+              <Menu.Item as={Link} to={'/battle'}>
+                  Start New Game
+              </Menu.Item>
+              </Menu.Menu>
+               <Menu.Menu position="right">
+              <Menu.Item onClick={this.props.handleLogout} as={Link} to="/">
+                  Logout
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu> */}
+    
+      
+     </div>
+    )
+  }
+}
+export default NavBar
